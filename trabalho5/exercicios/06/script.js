644c081ffe919ca4bb2b7eae8feca2a7d63912e8
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-02-11 17:06:51 -0300

    Exercício 06

---
+++ trabalho5/exercicios/06/script.js
@@ -0,0 +1,35 @@
+function goBack() {
+    location.href = '../../';
+}
+
+function getIMCClassification(imc) {
+    if (imc < 18.5) {
+        return "Magreza";
+    } else if (imc < 25) {
+        return "Normal";
+    } else if (imc < 30) {
+        return "Sobrepeso";
+    } else if (imc < 40) {
+        return "Obesidade";
+    } else {
+        return "Obesidade grave";
+    }
+}
+
+function calculateIMC() {
+    let numbersElements = document.getElementsByClassName('number-input');
+    let weight = 0, height = 0;
+    for (numberElement of numbersElements) {
+        if (numberElement.id == "weight") {
+            weight = Number(numberElement.value);
+        } else if (numberElement.id == "height") {
+            height = Number(numberElement.value);
+        }
+    }
+
+    let imc = weight / (height ** 2);
+
+    document.getElementById('imc').querySelector('h5').innerText = imc.toFixed(2);
+    document.getElementById('imc-classification').innerText = getIMCClassification(imc);
+
+}
