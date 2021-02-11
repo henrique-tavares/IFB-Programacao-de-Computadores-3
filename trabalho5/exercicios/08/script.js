f956cb1bc3ec611eb116031e42ab09cd5088f3dd
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-02-11 17:53:38 -0300

    Exercício 08

---
+++ trabalho5/exercicios/08/script.js
@@ -0,0 +1,21 @@
+function goBack() {
+    location.href = '../../';
+}
+
+function fibonacci(num) {
+    if (num <= 0) {
+        return [ 0 ];
+    } else if (num == 1) {
+        return [ 0, 1 ];
+    }
+    let prev = fibonacci(num - 1);
+    return [ ...prev, prev[ prev.length - 1 ] + prev[ prev.length - 2 ] ];
+}
+
+function fibonacciSum() {
+    let quantity = document.getElementById('quantity').value;
+    let fib = fibonacci(quantity);
+    let sum = fib.reduce((a, b) => a + b);
+
+    document.getElementById('fib-result').innerText = sum;
+}
