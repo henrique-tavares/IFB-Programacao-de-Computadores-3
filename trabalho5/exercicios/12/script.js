9bf104b649b51d71f98a1673aa6a4b4f8d808603
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-02-11 19:38:49 -0300

    Exercício 12

---
+++ trabalho5/exercicios/12/script.js
@@ -0,0 +1,14 @@
+function goBack() {
+    location.href = '../../';
+}
+
+function reverseString() {
+    const text = document.getElementById('string').value;
+    let reversedText = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        reversedText += text[ i ];
+    }
+
+    document.getElementById('result').innerText = reversedText;
+}
