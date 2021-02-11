8d28ae6eed99f9be5dab02a153c74e4b7bc285d5
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-02-11 17:06:38 -0300

    Exercício 04

---
+++ trabalho5/exercicios/04/script.js
@@ -0,0 +1,9 @@
+function goBack() {
+    location.href = '../../';
+}
+
+function verifyAge() {
+    let age = document.getElementById('age').value;
+
+    document.getElementById('age-result').innerText = (age >= 18) ? "Você é maior de idade" : "Você não é maior de idade";
+}
